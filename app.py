2543d3ad5eab4a9bc098368ed7c1c1f175b05dc4
nirmal1311 <regsite1311@gmail.com>
2024-02-12 15:07:49 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask,render_template,url_for,request,redirect,session
-from database import get_database
-from werkzeug.security import  generate_password_hash,check_password_hash
+from flask import Flask,render_template,url_for,request,redirect
 import google.generativeai as genai
 from dotenv import load_dotenv
 from os import getenv
